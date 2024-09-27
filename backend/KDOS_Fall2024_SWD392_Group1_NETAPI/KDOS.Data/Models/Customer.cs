511044bb158d6a95503de8bf6ba061ac800a8665
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-09-27 13:54:19 +0700

    MockTestAPI Customer

--- backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS.Data/Models/Customer.cs
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS.Data/Models/Customer.cs
@@ -9,6 +9,10 @@ namespace KDOS.Data.Models
     public class Customer
     {
         public Guid CustomerId { get; set; }
-
+        public required String CustomerName { get; set; }
+        public required int Age { get; set; }
+        public required String Email { get; set; }
+        public required String[] Addresses { get; set; }
     }
+    
 }
---
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KDOS.Data.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KDOS_Web_API.Controllers
+{
+    // Route: https//localhost:portnumber /api/customers
+    [Route("api/[controller]API")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            //string[] customerName = new string[] { "Reimu", "Marisa", "Sakuya" };
+            //return Ok(customerName);
+            var customerList = new List<Customer>
+            { new Customer
+                {
+                    CustomerId = Guid.NewGuid(),
+                    CustomerName = "Hakurei Reimu",
+                    Addresses= new string[]{"Hakurei Shrine, Gensokyo"},
+                    Age=19,
+                    Email="hakureimoney@gensokyo.com",
+
+
+                },
+                new Customer
+                {
+                    CustomerId = Guid.NewGuid(),
+                    CustomerName = "Sakuya Izayoi",
+                    Addresses= new string[]{"S.D.M, Gensokyo"},
+                    Age=23,
+                    Email="inusakuya@gensokyo.com",
+                }
+            };
+            return Ok(customerList);
+
+        }
+    }
+}
