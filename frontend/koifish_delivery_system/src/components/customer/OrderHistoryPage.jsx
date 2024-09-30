0925de7f1a569bf23e238e77fea0bcded52bf2e3
anhhoangg6 <hoanganh0628@gmail.com>
2024-09-30 13:50:31 +0700

    update: signup,order history

--- frontend/koifish_delivery_system/src/components/customer/OrderHistoryPage.jsx
+++ frontend/koifish_delivery_system/src/components/customer/OrderHistoryPage.jsx
@@ -1,65 +1,73 @@
+import { DataGrid } from '@mui/x-data-grid';
+
 import '../../css/OrderHistoryPage.css';
-import OrderItem from './OrderItem';
+
+const columns = [
+  { field: 'id', headerName: 'Order ID', width: 150 },
+  { field: 'orderDates', headerName: 'Order Dates', width: 150 },
+  { field: 'deliveryPrice', headerName: 'Delivery Price', width: 150 },
+  {
+    field: 'dateShipmentArrived',
+    headerName: 'Date Shipment Arrived',
+    width: 200,
+  },
+  {
+    field: 'orderStatus',
+    headerName: 'Order Status',
+    width: 150,
+  },
+];
 
 const OrderHistoryPage = () => {
+  const paginationModel = { page: 0, pageSize: 5 };
   const fakeData = [
     {
-      orderId: '734538957343',
+      id: '734538957343',
       orderDates: '28/09/2024',
       deliveryPrice: 20000,
       dateShipmentArrived: '12/10/2025',
       orderStatus: 'Deliveried',
     },
     {
-      orderId: '157684956738',
+      id: '157684956738',
       orderDates: '28/09/2024',
       deliveryPrice: 20000,
       dateShipmentArrived: '30/09/2024',
-      orderStatus: 'Cancle',
+      orderStatus: 'Cancel',
     },
     {
-      orderId: '748395647385',
+      id: '748395647385',
       orderDates: '28/09/2024',
       deliveryPrice: 20000,
       dateShipmentArrived: '30/09/2024',
       orderStatus: 'Deliveried',
     },
     {
-      orderId: '458496847564',
+      id: '458496847564',
       orderDates: '28/09/2024',
       deliveryPrice: 20000,
       dateShipmentArrived: '30/09/2024',
       orderStatus: 'Deliveried',
     },
     {
-      orderId: '564758694657',
+      id: '564758694657',
       orderDates: '28/09/2024',
       deliveryPrice: 20000,
       dateShipmentArrived: '30/09/2024',
       orderStatus: 'Deliveried',
     },
-
-
-
   ];
 
   return (
     <div className='order-history-page-container'>
       <p className='order-history-title'>Your Order History</p>
       <div className='list-order-container'>
-        {fakeData.map((data) => {
-          return (
-            <OrderItem
-              key={data.orderId}
-              orderId={data.orderId}
-              orderDates={data.orderDates}
-              deliveryPrice={data.deliveryPrice}
-              dateShipmentArrived={data.dateShipmentArrived}
-              orderStatus={data.orderStatus}
-
-            />
-          );
-        })}
+        <DataGrid
+          rows={fakeData}
+          columns={columns}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[5, 10]}
+        />
       </div>
     </div>
   );
