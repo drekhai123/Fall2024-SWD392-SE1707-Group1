4da3c652857cb27e7dbe3398afe53ee6e3389649
vietdung <vovietdung20013007@gmail.com>
2024-09-30 23:50:17 +0700

    -- Dung --: update order form

--- frontend/koifish_delivery_system/src/App.js
+++ frontend/koifish_delivery_system/src/App.js
@@ -1,7 +1,6 @@
+import {RouterProvider} from 'react-router-dom';
 
-import { RouterProvider } from 'react-router-dom';
-
-import { router } from './routes';
+import {router} from './routes';
 
 function App() {
   return <RouterProvider router={router} />;
