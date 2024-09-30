70212291d4fa05d4825cf0709a2ca30d3cb2752c
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-09-30 23:43:42 +0700

    Staff Controller

--- backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Controllers/CustomerController.cs
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Controllers/CustomerController.cs
@@ -25,14 +25,14 @@ namespace KDOS_Web_API.Controllers
         public IActionResult GetAllCustomer()
         {
             // This method get data DIRECTLY from database -> not best practice
-           var customerList = customerContext.Customer.ToList();
+            var customerList = customerContext.Customer.ToList();
             var customerDto = new List<CustomerDTO>();
-            foreach(Customer customer in customerList)
+            foreach (Customer customer in customerList)
             {
                 customerDto.Add(new CustomerDTO()
                 {
-                    CustomerId=customer.CustomerId,
-                    CustomerName=customer.CustomerName,
+                    CustomerId = customer.CustomerId,
+                    CustomerName = customer.CustomerName,
                     Address = customer.Address,
                     Age = customer.Age,
                     Email = customer.Email,
@@ -45,7 +45,7 @@ namespace KDOS_Web_API.Controllers
         }
 
         [HttpPost] // Post for create - FromBody is the data send from Client in the Respone Body
-        public IActionResult AddNewCustomer([FromBody] AddNewCustomerDTO customer )
+        public IActionResult AddNewCustomer([FromBody] AddNewCustomerDTO customer)
         {
             // using the DTO to convert Model
             var customerModel = new Customer
@@ -73,11 +73,38 @@ namespace KDOS_Web_API.Controllers
                 PhoneNumber = customerModel.PhoneNumber
             };
             // Follow best practice
-            return CreatedAtAction(nameof(GetCustomerById),new { customerId = customerModel.CustomerId }, customerDto) ; // Respone with code 201 - Created Complete
+            return CreatedAtAction(nameof(GetCustomerById), new { customerId = customerModel.CustomerId }, customerDto); // Respone with code 201 - Created Complete
             //CreatedAtAction will trigger the action GetCustomerById to search for the created customer in the db using the id generate by the EF. Then convert the data to a DTO and respone that bakc to client. So we can know what dot created 
         }
-       
-      
+        // TODO
+        //[HttpGet]
+        //[Route("{customerName}")]
+        //public IActionResult FindCustomerByName([FromBody] String customerName)
+        //{
+        //    //Find by name
+        //    var customerModel = customerContext.Customer.F; // enforce ! to make sure name is not null
+        //    if (customerModel == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        //Turn Model to DTO
+        //        var customerDto = new CustomerDTO
+        //        {
+        //            CustomerId = customerModel.CustomerId,
+        //            CustomerName = customerModel.CustomerName,
+        //            Address = customerModel.Address,
+        //            Age = customerModel.Age,
+        //            Email = customerModel.Email,
+        //            Gender = customerModel.Gender,
+        //            PhoneNumber = customerModel.PhoneNumber
+        //        };
+        //        return Ok(customerDto);
+        //    }
+        //}
+
+
 
         // GET 1 customer by ID
         [HttpGet]
@@ -87,21 +114,26 @@ namespace KDOS_Web_API.Controllers
             //var customer = customerContext.Customer.Find(customerId); // Method will only work with fidning [Key] like Id
             var customer = customerContext.Customer.FirstOrDefault(x => x.CustomerId == customerId); // Method will  work with ALL property you want to seach: name, add, phone,
             //follow best practice
-            var customerDto = new CustomerDTO
-            {
-                CustomerId = customer!.CustomerId,
-                CustomerName = customer.CustomerName,
-                Address = customer.Address,
-                Age = customer.Age,
-                Email = customer.Email,
-                Gender = customer.Gender,
-                PhoneNumber = customer.PhoneNumber
-            };
             if (customer == null)
             {
                 return NotFound(); //return 404
             }
-            return Ok(customerDto); //return 200 ok
+            else
+            {
+                var customerDto = new CustomerDTO
+                {
+                    CustomerId = customer!.CustomerId,
+                    CustomerName = customer.CustomerName,
+                    Address = customer.Address,
+                    Age = customer.Age,
+                    Email = customer.Email,
+                    Gender = customer.Gender,
+                    PhoneNumber = customer.PhoneNumber
+                };
+
+                return Ok(customerDto); //return 200 ok
+            }
+           
         }
         // PUT - Update a customer through their Id
         [HttpPut]
@@ -135,13 +167,37 @@ namespace KDOS_Web_API.Controllers
                     Gender = customerModel.Gender,
                     PhoneNumber = customerModel.PhoneNumber
                 };
-                return Ok();
+                return Ok(customerDto);
             }
         }
-
-
-
-
+        [HttpDelete]
+        [Route("{customerId}")]
+        public IActionResult DeleteCustomer([FromHeader] int customerId)
+        {
+            var deleteCustomer = customerContext.Customer.FirstOrDefault(x => x.CustomerId == customerId);
+            if (deleteCustomer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                customerContext.Customer.Remove(deleteCustomer);
+                customerContext.SaveChanges();
+                //Return the customer info got deleted back
+                var customerDto = new CustomerDTO
+                {
+                    CustomerId = deleteCustomer.CustomerId,
+                    CustomerName = deleteCustomer.CustomerName,
+                    Address = deleteCustomer.Address,
+                    Age = deleteCustomer.Age,
+                    Email = deleteCustomer.Email,
+                    Gender = deleteCustomer.Gender,
+                    PhoneNumber = deleteCustomer.PhoneNumber
+                };
+                return Ok(customerDto);
+            }
+            
+        }
     }
 }
 
