2c381ccc6807e8e753ba0b9694c62a88eb5f08b7
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-10-03 23:56:33 +0700

    Repository

---
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Repositories/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using KDOS_Web_API.Models.Domains;
+
+namespace KDOS_Web_API.Repositories
+{
+	// The terface is for all the action to work with in a API
+	public interface IAccountRepository
+	{
+		Task<List<Account>> GetAllAccountAsync();
+	}
+}
+
