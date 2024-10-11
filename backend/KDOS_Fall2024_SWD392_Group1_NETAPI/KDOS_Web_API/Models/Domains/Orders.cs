59a7b3f75a83235d8ab1e9ffe860eac22eba25f2
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-10-11 16:59:58 +0700

    Update Order DOMAIN
    Domain Expansion

--- backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Models/Domains/Orders.cs
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Models/Domains/Orders.cs
@@ -10,23 +10,26 @@ namespace KDOS_Web_API.Models.Domains
         [Key]
         public int OrderId { get; set; }
 
-        public string? RecipientAddress { get; set; }
-        public string? RecipientName { get; set; }
-        public string? RecipientPhoneNumber { get; set; }
-        public string? RecipientEmail { get; set; }
-        public PaymentMethod PaymentMethod { get; set; }
-        public PaymentStatus PaymentStatus { get; set; }
-        public OrderStatus DeliveryStatus { get; set; }
-        public string? DeliveryNote { get; set;}
-        public int Quantity { get; set; }
-        public double TotalWeight { get; set; }
-        public double TotalCost { get; set; }
-        public int OrderStatus { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        required public String SenderName { get; set; }
+        required public String SenderAddress { get; set; }
+        required public String SenderPhoneNumber { get; set; }
+        required public String RecipientAddress { get; set; }
+        required public String RecipientName { get; set; }
+        required public String RecipientPhoneNumber { get; set; }
+        required public String RecipientEmail { get; set; }
+        required public PaymentMethod PaymentMethod { get; set; }
+        required public PaymentStatus PaymentStatus { get; set; }
+        required public OrderStatus DeliveryStatus { get; set; }
+        required public string? DeliveryNote { get; set;}
+        required public int Quantity { get; set; }
+        required public double TotalWeight { get; set; }
+        required public double TotalCost { get; set; }
+        required public int OrderStatus { get; set; }
+        required public DateTime CreatedAt { get; set; }
+        required public DateTime UpdatedAt { get; set; }
 
         // Relationships
-        public int CustomerId { get; set; }
+        required public int CustomerId { get; set; }
         public Customer Customer { get; set; } = null!; // REQUIRED Many-to-one relationship with Customer
         public ICollection<OrderDetails> OrderDetails { get; set; } = new List<OrderDetails>(); // One-to-many relationship with OrderDetails
         public int TransportId { get; set; } // Foreign key to transport. 1 transport can have Many Order
