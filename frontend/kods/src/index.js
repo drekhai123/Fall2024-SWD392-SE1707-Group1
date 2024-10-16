2f711e82520e0245d4c175c09cf0ec0f7e95bcb2
anhhoangg6 <hoanganh0628@gmail.com>
2024-10-17 00:09:51 +0700

    login-signup

--- frontend/kods/src/index.js
+++ frontend/kods/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import SnackbarProviders from "./provider/notification";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SnackbarProviders>
       <App />
+      <ToastContainer />
     </SnackbarProviders>
   </React.StrictMode>
 );
