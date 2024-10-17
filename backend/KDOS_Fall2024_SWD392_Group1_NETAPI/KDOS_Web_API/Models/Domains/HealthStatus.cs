b4ee542180cb50834788b3651f8b5eb6c326934b
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-10-17 19:07:25 +0700

    Updated HealthStatus

--- backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Models/Domains/HealthStatus.cs
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Models/Domains/HealthStatus.cs
@@ -9,7 +9,10 @@ namespace KDOS_Web_API.Models.Domains
         public int HealthStatusId { get; set; }
         required public DateTime Date { get; set; }
         required public String Status { get; set; }
-        required public String Description { get; set; }
+        required public float Temperature { get; set; }
+        required public float OxygenLevel { get; set; }
+        required public float pHLevel { get; set; }
+        required public String Notes { get; set; }
         // Relationship
         public int OrderDetailsId { get; set; }
         public OrderDetails OrderDetails { get; set; } = null!; // REQUIRED relationship
