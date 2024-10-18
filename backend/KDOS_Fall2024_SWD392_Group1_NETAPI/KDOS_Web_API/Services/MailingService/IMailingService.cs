fd0403db2e011d56cf995044dd01c7910bf13442
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-10-18 11:51:53 +0700

    Add mailing service Interface

---
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Services/MailingService/IMailingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using KDOS_Web_API.Models.Domains;
+
+namespace KDOS_Web_API.Services.MailingService
+{
+	public interface IMailingService
+	{
+		Task RegisterMail(Account account);
+        Task OrderConfirmation(Account account, Orders orders);
+        Task EnvoiceMail(Account account,Orders orders);
+        //TODO
+        Task ResetPassword(Account account);
+    }
+}
+
---
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Services/MailingService/MailingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using KDOS_Web_API.Models.Domains;
+
+namespace KDOS_Web_API.Services.MailingService
+{
+    public class MailingService : IMailingService
+    {
+       public async Task EnvoiceMail(Account account, Orders orders)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task OrderConfirmation(Account account, Orders orders)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RegisterMail(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ResetPassword(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+
