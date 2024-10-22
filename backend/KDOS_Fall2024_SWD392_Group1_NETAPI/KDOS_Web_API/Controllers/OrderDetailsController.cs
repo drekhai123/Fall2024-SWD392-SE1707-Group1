eb04a84226bb8b347d361fa77b43b55f663c7108
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-10-22 16:28:41 +0700

    OrderDetails controller

--- backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Controllers/OrderDetailsController.cs
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Controllers/OrderDetailsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using KDOS_Web_API.Models.Domains;
+using KDOS_Web_API.Models.DTOs;
 using KDOS_Web_API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +12,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace KDOS_Web_API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class OrderDetailsController : ControllerBase
     {
         private readonly IOrderDetailsRepository orderDetailsRepository;
@@ -20,7 +24,69 @@ namespace KDOS_Web_API.Controllers
             this.orderDetailsRepository = orderDetailsRepository;
             this.mapper = mapper;
         }
+        [HttpPost]
+        public async Task<IActionResult> AddNewOrderDetails([FromBody] AddNewOrderDetailsDTO addNewOrderDetailsDTO)
+        {
+            var detailsModel = mapper.Map<OrderDetails>(addNewOrderDetailsDTO);
+            detailsModel = await orderDetailsRepository.AddNewOrderDetails(detailsModel);
+            if (detailsModel == null)
+            {
+                return NotFound();
+            }
+            var newOrderDetails = mapper.Map<OrderDetailsDTO>(detailsModel);
+            return CreatedAtAction(nameof(GetOrderDetailsById), new { detailsId = detailsModel.OrderDetailsId }, newOrderDetails);
+        }
+        [HttpDelete]
+        [Route("{detailsId}")]
+        public async Task<IActionResult> DeleteKoiFish([FromRoute] int detailsId)
+        {
 
+            var detailsModel = await orderDetailsRepository.DeleteOrderDetails(detailsId);
+            if (detailsModel == null)
+            {
+                return NotFound();
+            }
+            var detailsDto = mapper.Map<KoiFishDTO>(detailsModel);
+            return Ok(detailsDto);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrderDetails()
+        {
+            var detailsModel = await orderDetailsRepository.GetAllOrderDetails();
+            var detailsDto = mapper.Map<List<OrderDetailsDTO>>(detailsModel);
+            return Ok(detailsDto);
+        }
+
+        [HttpGet]
+        [Route("{detailsId}")]
+        public async Task<IActionResult> GetOrderDetailsById([FromRoute] int detailsId)
+        {
+            var detailsModel = await orderDetailsRepository.GetOrderDetailsById(detailsId);
+            if (detailsModel == null)
+            {
+                return NotFound();
+            }
+            var detailsDto = mapper.Map<OrderDetailsDTO>(detailsModel);
+            return Ok(detailsDto);
+        }
+        [HttpGet]
+        [Route("Order/{orderId}")]
+        public async Task<IActionResult> GetOrderDetailsByOrderId([FromRoute] int orderId)
+        {
+            var detailsModel = await orderDetailsRepository.GetOrderDetailsByOrderId(orderId);
+            if (detailsModel == null)
+            {
+                return NotFound();
+            }
+            var detailsDto = mapper.Map<List<OrderDetailsDTO>>(detailsModel);
+            return Ok(detailsDto);
+        }
+        [HttpPut]
+        [Route("{koiFishId}")]
+        public IActionResult UpdateKoiFish()
+        {
+            return BadRequest("PUT method is not supported");
+        }
     }
 }
 
