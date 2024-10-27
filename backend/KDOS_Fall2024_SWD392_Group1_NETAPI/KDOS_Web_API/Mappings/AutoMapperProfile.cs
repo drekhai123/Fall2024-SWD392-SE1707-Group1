b9fed232925e0ca187b8d1e1efbf4236286860cc
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-10-27 18:10:01 +0700

    Fix Add New Account

--- backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Mappings/AutoMapperProfile.cs
+++ backend/KDOS_Fall2024_SWD392_Group1_NETAPI/KDOS_Web_API/Mappings/AutoMapperProfile.cs
@@ -16,9 +16,10 @@ namespace KDOS_Web_API.Mappings
                 // CreateMap<Verification, VerificationDTO>().ReverseMap(); Don't think of using this
 			// Account Mapping
 			CreateMap<Account, AccountDTO>().ReverseMap();
-			CreateMap<AddNewAccountDTO, Account>().ReverseMap()
+			CreateMap<Account, AddNewAccountDTO>().ReverseMap()
 				.ForMember(x => x.Password, option => option.Ignore())
-				.ForMember(x => x.Banned, option => option.Ignore())// Manually set the ban as false
+                .ForMember(x => x.Role, option => option.Ignore())
+                .ForMember(x => x.Banned, option => option.Ignore())// Manually set the ban as false
 				.ForMember(x=>x.Role, option => option.Ignore());// Manually set the role dependent on the need
 			CreateMap<Account, UpdateAccountStatus>().ReverseMap();
 			CreateMap<UpdateAccountDTO, Account>().ReverseMap();
