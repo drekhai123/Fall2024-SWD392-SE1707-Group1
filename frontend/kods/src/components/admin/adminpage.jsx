72d693a5c7307c0f70d2de8b866a1d2b9a54aba6
Doan Tuan Khai <khai.lumberjack@gmail.com>
2024-11-05 02:08:22 +0700

    Mange Staff, Delivery Staff, Customers

--- frontend/kods/src/components/admin/adminpage.jsx
+++ frontend/kods/src/components/admin/adminpage.jsx
@@ -4,28 +4,29 @@ import React, { useState } from 'react';
 // import ManageRoute from './manageroute';
 import '../../css/manager.css';
 import AccountManager from './accountmanager';
-
+import DeliveryStaffManager from './deliverystaffmanager';
+import StaffManager from './staffmanager';
+import CustomerManager from './customermanager';
 
 function AdminPage() {
   const [activeComponent, setActiveComponent] = useState('accountmanagement');
   const [selectedAccountId, setSelectedAccountId] = useState(null); // New state for selected account
-
-
 
   const handleAccountClick = (accountId) => { // Updated function for account management
     setSelectedAccountId(accountId);
     setActiveComponent(accountId);
   };
 
-
-
   const renderContent = () => {
     switch (activeComponent) {
-
       case 'accountmanagement': // New case for account management
         return <AccountManager selectedAccountId={selectedAccountId} />;
-
-
+      case 'deliverystaffmanagement': // New case for delivery staff management
+        return <DeliveryStaffManager selectedAccountId={selectedAccountId} />;
+      case 'staffmanagement': // New case for staff management
+        return <StaffManager selectedAccountId={selectedAccountId} />;
+      case 'customermanagement': // New case for customer management
+        return <CustomerManager selectedAccountId={selectedAccountId} />;
       default:
         return (
           <div>
@@ -45,6 +46,21 @@ function AdminPage() {
             <li>
               <button onClick={() => handleAccountClick('accountmanagement')} className={activeComponent === 'accountmanagement' ? 'active' : ''}>
                 Manage Accounts
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleAccountClick('deliverystaffmanagement')} className={activeComponent === 'deliverystaffmanagement' ? 'active' : ''}>
+                Manage Delivery Staff
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleAccountClick('staffmanagement')} className={activeComponent === 'staffmanagement' ? 'active' : ''}>
+                Manage Staff
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleAccountClick('customermanagement')} className={activeComponent === 'customermanagement' ? 'active' : ''}>
+                Manage Customers
               </button>
             </li>
           </ul>
